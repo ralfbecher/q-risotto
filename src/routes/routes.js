7f7e7b7c266e4ba0cf224a08600f1987a55b30d2
Ralf Becher <ralf.becher@web.de>
2017-12-22 13:24:32 +0100

    dateFromQlikNumber round fix

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -393,7 +393,7 @@ module.exports.routes = [
 
 function dateFromQlikNumber(n) {
     // return: Date from QlikView number
-    var d = new Date((n - 25569) * 86400 * 1000);
+    var d = new Date(Math.round((n - 25569) * 86400 * 1000));
     // since date was created in UTC shift it to the local timezone
     d.setTime(d.getTime() + d.getTimezoneOffset() * 60 * 1000);
     return d;
