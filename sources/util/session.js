e00050ce8e0b4f101761c6c63d78abb282e813cf
Ralf Becher <ralf.becher@web.de>
2018-05-31 16:51:53 +0200

    improved session handling

--- sources/util/session.js
+++ sources/util/session.js
@@ -6,15 +6,20 @@ const config = require('../src/config/config');
 
 const schema = require(config.enigmaSchema);
 
-module.exports = () => enigma.create({
-  schema,
-  url: `wss://${config.engineHost}:${config.enginePort}/app/${config.appId}`,
-  createSocket: url => new WebSocket(url, {
-    ca: [config.certificates.ca],
-    key: config.certificates.key,
-    cert: config.certificates.cert,
-    headers: {
-      'X-Qlik-User': `UserDirectory=${encodeURIComponent(config.userDirectory)}; UserId=${encodeURIComponent(config.userId)}`,
-    }
-  })
-});
+module.exports = (appId) => {
+  if (!appId || appId === '') {
+    appId = config.globalAppId;
+  }
+  return enigma.create({
+    schema,
+    url: `wss://${config.engineHost}:${config.enginePort}/app/${appId}`,
+    createSocket: url => new WebSocket(url, {
+      ca: [config.certificates.ca],
+      key: config.certificates.key,
+      cert: config.certificates.cert,
+      headers: {
+        'X-Qlik-User': `UserDirectory=${encodeURIComponent(config.userDirectory)}; UserId=${encodeURIComponent(config.userId)}`,
+      }
+    })
+  });
+}
